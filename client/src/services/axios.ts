637c0a2a3b270224c9047f3220247c1af9231115
Teyvani <152383191+Teyvani@users.noreply.github.com>
2026-02-04 15:20:00 +0200

    Honestly, I have no idea what im doing there, but ill keep trying

---
+++ client/src/services/axios.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.BACKEND_URL,
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("purr_cards");
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
